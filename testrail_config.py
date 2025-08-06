f1184bdd460a9854feefd87da255412a9792a40e
ahmadshiddiq <ahmad.shiddiq@digits.id>
2025-08-06 13:42:25 +0700

    update setup testrail

--- testrail_config.py
+++ testrail_config.py
@@ -1,12 +1,11 @@
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 import os
 
-load_dotenv()
+#load_dotenv()
 
 #testrail_config.py
-TESTRAIL_URL = os.getenv('TESTRAIL_URL')
+TESTRAIL_URL = os.getenv('TESTRAIL_URL', 'https://toha.testrail.io/')
 TESTRAIL_EMAIL = os.getenv('TESTRAIL_EMAIL')
 TESTRAIL_API_KEY = os.getenv('TESTRAIL_API_KEY')
 TESTRAIL_PROJECT_ID = os.getenv('TESTRAIL_PROJECT_ID', "1")
 TESTRAIL_RUN_ID = os.getenv('TESTRAIL_RUN_ID', "17")
-
